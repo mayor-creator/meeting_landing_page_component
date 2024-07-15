5dc656f47e6e72c53a951a2e24ef4c551ddbca27
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-07-15 08:37:07 -0400

    add css stylesheet properties

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,12 +5,14 @@ import imageHeroLeft from "../../assets/desktop/image-hero-left.png";
 import imageHeroRight from "../../assets/desktop/image-hero-right.png";
 import imageHero from "../../assets/tablet/image-hero.png";
 
+import style from "./Header.module.css";
+
 export function Header() {
 	return (
 		<>
 			<header>
 				<nav>
-					<div>
+					<div className={style.logoContainer}>
 						<img
 							className="logo"
 							src={logo}
@@ -18,7 +20,8 @@ export function Header() {
 						/>
 					</div>
 				</nav>
-				<div className="laptop-container">
+
+				<div className={style.laptopContainer}>
 					<div className="hero-left-container">
 						<img
 							src={imageHeroLeft}
@@ -44,21 +47,26 @@ export function Header() {
 					</div>
 				</div>
 
-				<div className="tablet-mobile-container">
-					<div className="hero-image">
+				<div className={style.tabletMobileContainer}>
+					<div>
 						<img
+							className={style.heroImage}
 							src={imageHero}
 							alt="portrait of different people"
 						/>
-						<div className="group-chat">
-							<h1>Group Chat for Everyone</h1>
-							<p>
+						<div className={style.groupChatContainer}>
+							<h1 className={style.groupChatHeader}>Group Chat for Everyone</h1>
+							<p className={style.groupChatMessage}>
 								Meet makes it easy to connect with others face-to-face virtually
 								and collaborate across any device.
 							</p>
 							<div className="buttons">
-								<Button>Download v1.3</Button>
-								<Button>What is it?</Button>
+								<Button className={style.groupChatButtonVersion}>
+									Download <span className={style.spanVersion}>v1.3</span>
+								</Button>
+								<Button className={style.groupChatButtonQuestion}>
+									What is it?
+								</Button>
 							</div>
 						</div>
 					</div>
