afe6f4c4923d166f9d459efe488263448942b76f
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-07-15 23:03:06 -0400

    add stylesheet name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,23 @@
 import { Button } from "../Button";
+import style from "./Footer.module.css";
 
 export function Footer() {
 	return (
 		<>
-			<footer>
-				<div className="vertical-line"></div>
-				<div>
-					<p>02</p>
+			<footer className={style.footer}>
+				<div className={style.lineContainer}>
+					<div className={style.verticalLine}></div>
+					<div className={style.circle}>
+						<p className={style.circleVersion}>02</p>
+					</div>
 				</div>
-				<div>
-					<h3>Experience more together</h3>
-					<p>
+				<div className={style.footerContainer}>
+					<h3 className={style.footerHeader}>Experience more together</h3>
+					<p className={style.footerMessage}>
 						Stay connected with reliable HD meetings and unlimited one-on-one
 						and group video sessions.
 					</p>
-					<Button>Download v1.3</Button>
+					<Button className={style.footerButtonVersion}>Download v1.3</Button>
 				</div>
 			</footer>
 		</>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,44 +3,54 @@ import imageWomenVideoCHat from "../../assets/desktop/image-women-videochatting.
 import imageMenInMeeting from "../../assets/desktop/image-men-in-meeting.jpg";
 import imageManTexting from "../../assets/desktop/image-man-texting.jpg";
 
+import style from "./Main.module.css";
+
 export function Main() {
 	return (
 		<>
 			<main>
-				<div className="vertical-line"></div>
-				<div className="circle">
-					<p>01</p>
+				<div className={style.lineContainer}>
+					<div className={style.verticalLine}></div>
+					<div className={style.circle}>
+						<p className={style.circleVersion}>01</p>
+					</div>
 				</div>
-				<div className="image-container">
+				<div className={style.imageContainer}>
 					<div>
 						<img
+							className={style.item}
 							src={imageWomanInVideoCall}
 							alt="a woman in video call"
 						/>
 					</div>
 					<div>
 						<img
+							className={style.item}
 							src={imageWomenVideoCHat}
 							alt="women in video chat"
 						/>
 					</div>
 					<div>
 						<img
+							className={style.item}
 							src={imageMenInMeeting}
 							alt="men in meeting"
 						/>
 					</div>
 					<div>
 						<img
+							className={style.item}
 							src={imageManTexting}
 							alt="man texting on a phone"
 						/>
 					</div>
 				</div>
-				<div>
-					<p>Built for modern use</p>
-					<h2>Smarter meetings, all in one place</h2>
-					<p>
+				<div className={style.modernContainer}>
+					<h2 className={style.modernBuilt}>Built for modern use</h2>
+					<p className={style.modernHeader}>
+						Smarter meetings, all in one place
+					</p>
+					<p className={style.modernMessage}>
 						Send messages, share files, show your screen, and record your
 						meetings — all in one workspace. Control who can join with
 						invite-only team access, data encryption, and data export.
